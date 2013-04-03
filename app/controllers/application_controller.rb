3647c823fed5e9f3951f574d73c6b0c263d738bb
Kristian Mandrup <kmandrup@gmail.com>
2013-04-03 16:59:18 +0200

    use application controller for main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,41 @@
 class ApplicationController < ActionController::API
-  include JsonStatusCodeRender  
+  # include JsonStatusCodeRender
+
+  def index
+    puts "Hello world"
+  end
+
+  def render(*args)
+    puts "do render!"
+
+    obj = args.first
+    if no_status? obj
+      if resource_was_destroyed? obj
+        # 204 : request was received and understood, but no need to send any data back
+        args.first[:status] = 204
+      elsif resource_was_created? obj
+        # 201 Created: The request has been fulfilled and resulted in a new resource
+        args.first[:status] = 201
+      end      
+    end
+
+    puts "rendering!"
+
+    super
+  end
+
+  protected
+
+  def resource_was_destroyed? obj
+    obj[:json].respond_to?(:destroyed?) && obj[:json].destroyed?
+  end
+
+  def resource_was_created? obj
+    obj[:json].respond_to?(:created?) && obj[:json].created?
+  end
+
+  def no_status? obj
+    obj.key?(:json) && obj[:json].kind_of?(Mongoid::Document) && !obj.key?(:status)
+  end  
 end
 
